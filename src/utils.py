b2a8601e734eb22b36b291eca8212d5847b76364
Rachida <rachidaseghiripro@gmail.com>
2019-10-15 07:59:25 +0200

    add comments eda notebook

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 
 def data_path(*path):
     this_dir = os.path.realpath(os.path.dirname(__file__))
-    return os.path.join(this_dir, "..", "..", "data", *path)
+    return os.path.join(this_dir, "..", "data", *path)
 
 def split_features_target(df, target):
     return df.drop(target, axis=1), df[target]
