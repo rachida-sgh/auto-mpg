9e2c522308d3ea137d2a347d4b117349d093e869
Rachida <rachidaseghiripro@gmail.com>
2019-10-15 21:44:33 +0200

    debug transformation pipeline

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,14 +1,12 @@
+import os
 import pandas as pd
 import numpy as np
-import os
-
-from src.utils import data_path, split_features_target
-
-
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from src.utils import data_path
 
 # Continous features
 CONTINUOUS_FEATURES = ["displacement", "horsepower", "weight", "acceleration"]
@@ -45,24 +43,30 @@ def make_final_transformation_pipe():
     return full_pipeline
 
 
-def get_interim_data(dataset):
-    if dataset not in ["train", "test"]:
-        raise Exception("dataset type argument is train or test)")
-    filename = f"{dataset}_cleaned.pkl"
-    filepath = data_path("interim", filename)
-    return pd.read_pickle(filepath)
+# def get_interim_data(dataset):
+#     if dataset not in ["train", "test"]:
+#         raise Exception("dataset type argument is train or test)")
+#     filename = f"{dataset}_cleaned.pkl"
+#     filepath = data_path("interim", filename)
+#     return pd.read_pickle(filepath)
+
+
+def get_cleaned_train_test_df():
+    clean_data_path = data_path("interim", "data_cleaned.pkl")
+    df = pd.read_pickle(clean_data_path)
+    X = df.drop("mpg", axis=1)
+    y = df["mpg"]
+    return train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
 
 def make_final_sets():
-    df_train = get_interim_data("train")
-    df_test = get_interim_data("test")
-    X_train, y_train = split_features_target(df_train, "mpg")
-    X_test, y_test = split_features_target(df_test, "mpg")
+    X_train, X_test, y_train, y_test = get_cleaned_train_test_df()
 
     full_pipeline = make_final_transformation_pipe()
     X_train_processed_values = full_pipeline.fit_transform(X_train)
     X_test_processed_values = full_pipeline.transform(X_test)
-    # Add columns names to build the processed dataframe
+
+    # Add column names to build the processed dataframe
     region_ohe_features = list(
         full_pipeline.named_transformers_["nom"].get_feature_names()
     )
@@ -75,10 +79,10 @@ def make_final_sets():
         df.drop("x0_EUROPE", axis=1, inplace=True)
 
     # Save the data
-    df_train_processed = X_train_processed.join(y_train)
-    df_train_processed.to_csv(data_path("processed", "train_processed.pkl"))
+    df_train_processed = X_train_processed.join(y_train.reset_index(drop=True))
+    df_train_processed.to_pickle(data_path("processed", "train_processed.pkl"))
 
-    df_test_processsed = X_test_processed.join(y_test)
-    df_test_processsed.to_csv(data_path("processed", "test_processed.pkl"))
+    df_test_processsed = X_test_processed.join(y_test.reset_index(drop=True))
+    df_test_processsed.to_pickle(data_path("processed", "test_processed.pkl"))
 
     return df_train_processed, df_test_processsed
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,6 @@ import os
 
 def data_path(*path):
     this_dir = os.path.realpath(os.path.dirname(__file__))
-    print(this_dir)
     return os.path.join(this_dir, "..", "data", *path)
 
 def split_features_target(df, target):
