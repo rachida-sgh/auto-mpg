11df1d814e844decd1f121a2d53a6794b82c2fb2
Rachida <rachidaseghiripro@gmail.com>
2019-10-16 15:53:03 +0200

    refactor model selection and training

--- src/train.py
+++ src/train.py
@@ -2,7 +2,26 @@ import os
 import pandas as pd
 import numpy as np
 
+###### Report function for model selection and training
+def report_best_cv_scores(results):
+    # get the rank of the best model based on test score
+    best_model = np.flatnonzero(results["rank_test_score"] == 1)[0]
+    print("Best model scores")
+    print(
+        "Mean validation RMSE: {:.3f} (std: {:.3f})".format(
+            np.sqrt(-results["mean_test_score"][best_model]),
+            np.sqrt(results["std_test_score"][best_model]),
+        )
+    )
+    print(
+        "Mean train RMSE: {:.3f} (std: {:.3f})".format(
+            np.sqrt(-results["mean_train_score"][best_model]),
+            np.sqrt(results["std_train_score"][best_model]),
+        )
+    )
 
+
+###### Report function for algorithm selection
 def rmse_cross_validate(cv_results):
     df_errors = pd.DataFrame(columns=["rmse", "rmse_std"], index=["train", "test"])
     for set_ in ["train", "test"]:
@@ -12,27 +31,4 @@ def rmse_cross_validate(cv_results):
         df_errors.loc[set_, "rmse"] = rmse
         df_errors.loc[set_, "rmse_std"] = rmse_std
 
-    return df_errors.astype(float).round(3)
-
-
-
-# deprecated
-def errors_cv_multi_metrics(cv_results):
-    df_errors = pd.DataFrame(
-        columns=["rmse", "rmse_std", "r2", "r2_std"], index=["train", "test"]
-    )
-    for set_ in ["train", "test"]:
-        for error in ["mse", "r2"]:
-            cv_error_name = "{}_{}".format(set_, error)
-            error_colmn, std_error_colmn = get_colmn_names(error)
-            if error == "mse":
-                df_errors.loc[set_, error_colmn] = np.sqrt(
-                    cv_results[cv_error_name]
-                ).mean()
-                df_errors.loc[set_, std_error_colmn] = np.sqrt(
-                    cv_results[cv_error_name]
-                ).std()
-            else:
-                df_errors.loc[set_, error_colmn] = cv_results[cv_error_name].mean()
-                df_errors.loc[set_, std_error_colmn] = cv_results[cv_error_name].std()
     return df_errors.astype(float).round(3)
