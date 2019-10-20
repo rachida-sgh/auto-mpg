68c650c3e80e8ca9ca9398ffc678f1cbda8fffd3
Rachida <rachidaseghiripro@gmail.com>
2019-10-20 16:46:00 +0200

    add function to download data from uci ml

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,22 @@
 import os
+import urllib
 import matplotlib.pyplot as plt
+
+ROOT_DIR = ".."
 
 
 def data_path(*path):
     this_dir = os.path.realpath(os.path.dirname(__file__))
     return os.path.join(this_dir, "..", "data", *path)
+
+
+def download_data(file_name="auto-mpg.data"):
+    raw_data_dir = os.path.join(ROOT_DIR, "data", "raw")
+    os.makedirs(raw_data_dir, exist_ok=True)
+    file_path = data_path("raw",file_name)
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
+    urllib.request.urlretrieve(url, file_path)
+    print("downloading data to", os.path.join(raw_data_dir, "auto-mpg.data"))
 
 
 def save_fig(fig_path, fig_id, fig_extension="png"):
